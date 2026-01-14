52786bc0c4ef01c016cafc23bf6658fd53e7fab9
Vatojama <vatojama@gmail.com>
2026-01-14 13:23:54 +0100

    Add files via upload

---
+++ conexion_reportes/r_conexion.php
@@ -0,0 +1,13 @@
+<?php 
+$mysqli = new mysqli('tecnopvsertec.mysql.db','tecnopvsertec','Tecno18091975','tecnopvsertec');
+if (mysqli_connect_errno()) {
+	echo 'Conexion Fallida: ', mysqli_connect_error();
+	exit();
+	// code...
+}
+
+
+
+
+
+ ?>
